e22ec0cd4ea3194e90e70a2a3dfecd8b7d4c633e
Ilshin.Ji <Ilshin.Ji@peergroup.com>
2022-06-16 12:40:41 -0400

    Add project files.

---
+++ SimpleProfitDivider/BudgetItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleProfitDivider
+{
+    public class BudgetItem
+    {
+        public string Name { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
